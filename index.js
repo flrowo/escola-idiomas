f9587d5965ed538a7d81c4b364fd4b12be3399c3
unknown <joaopedrolibonati@gmail.com>
2019-10-01 05:45:35 +0000

    final commit?

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
