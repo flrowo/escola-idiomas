39b94b801f5ed54ce289b6c2c97fe7617d9c9e3c
unknown <funkymatoi@gmail.com>
2019-10-01 05:10:29 +0000

    final commit?

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # escola-idiomas
 college cloud computing homework, node.js with express application using image recognition, translator, and watson assistant from ibm cloud services
-disclaimer: some variables in portuguese...
+disclaimer: some variables and text in portuguese...
 
-it isn't working now (31/8)  
+log
+it isn't working now (31/08)
+now it is working (1/10)
+
 you need to create the file API_KEYs.js with your api keys like this:
 
-1  
-2 const imagem_API_KEY = '<YOUR_API_KEY>';  
-3 const tradutor_API_KEY = '<YOUR_API_KEY>';  
-4 const watson_API_KEY = '<YOUR_API_KEY>';  
-5  
+1 exports.imagem_API_KEY = "<YOUR_API_KEY>";
+2 exports.tradutor_API_KEY = "<YOUR_API_KEY>";
+3 exports.watson = {
+4     "API_KEY": "<YOUR_API_KEY>",
+5     "workspace_id": "<YOUR_API_KEY>",
+6     "assistant_id": "<YOUR_API_KEY>",
+7 };
+8 
+
+and add bodyParser if there is an error about 'middleware no longer bundled with Express'
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-
-
-var objeto = { "images": [ { "classifiers": [ { "classifier_id": "default", "name": "default", "classes": [ { "class": "sushi", "score": 0.999, "type_hierarchy": "/food/nutrition/dish/sushi" }, { "class": "dish", "score": 0.999 }, { "class": "nutrition", "score": 0.999 }, { "class": "food", "score": 0.999 }, { "class": "reddish orange color", "score": 0.998 } ] } ], "image": "0.jpg" } ], "images_processed": 1, "custom_classes": 0 };
-
-
-app.get('/test', (req, res) => {
-    res.send(objeto);
-});
 
 app.get('/gameSubmit/:img', (req, res) => {
     var VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
@@ -30,35 +25,6 @@ app.get('/gameSubmit/:img', (req, res) => {
         console.log(err);
     });
     
-});
-
-app.get('/traduzir/:texto', (req, res) => {
-    var texto = req.params.texto;
-
-    const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-    const tradutor_API_KEY = require('./API_KEYs').tradutor_API_KEY;
-    const languageTranslator = new LanguageTranslatorV3({
-    iam_apikey: tradutor_API_KEY,
-    url: 'https://gateway.watsonplatform.net/language-translator/api/',
-    version: '2018-08-07',
-    });
-
-    languageTranslator.translate({
-        text: texto,
-        source: 'en',
-        target: 'pt'
-    })
-    .then(translation => {
-        console.log("translation.translations[0].translation: "+translation.translations[0].translation + '; texto:'+texto);
-        var retorno = false;
-        if(translation.translations[0].translation == "text"){
-            retorno = true;
-        }
-        res.send('retorno: '+retorno)
-    })
-    .catch(err => {
-    console.log('error:', err);
-    });
 });
 
 app.get('/qtdImg', (req, res) => {
@@ -84,9 +50,9 @@ app.get('/ajuda/:imgid', (req, res) => {
         const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
         const tradutor_API_KEY = require('./API_KEYs').tradutor_API_KEY;
         const languageTranslator = new LanguageTranslatorV3({
-        iam_apikey: tradutor_API_KEY,
-        url: 'https://gateway.watsonplatform.net/language-translator/api/',
-        version: '2018-08-07',
+            iam_apikey: tradutor_API_KEY,
+            url: 'https://gateway.watsonplatform.net/language-translator/api/',
+            version: '2018-08-07',
         });
         var tempstring = result.images[0].classifiers[0].classes[0].class;
 
@@ -105,10 +71,34 @@ app.get('/ajuda/:imgid', (req, res) => {
     .catch(err => {
         console.log('error in visual recognition: '+err);
     });
-
-
-    
 });
+
+app.post('/mandarmsg', (req, res) => {
+    var input = req.body.msg;
+    var contexto = (req.body.idconversa == 1 ? "" : req.body.idconversa);
+    const watson = require('./API_KEYs').watson;
+
+    var AssistantV1 = require('ibm-watson/assistant/v1');
+    var assistant = new AssistantV1({
+        iam_apikey: watson.API_KEY,
+        url: 'https://gateway.watsonplatform.net/assistant/api/',
+        version: '2018-09-19'
+    });
+    assistant.message( {
+        input: { text: input },
+        assistant_id: watson.assistant_id,
+        workspace_id: watson.workspace_id,
+        context: { conversation_id: contexto }
+    })
+    .then(result => {
+        res.json(result, null, 2);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+});
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log("App Iniciada: http://localhost:" + port));
