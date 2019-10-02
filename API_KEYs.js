6367148fc2e57ba99d09d1c177656e46383b4331
João Pedro Libonati <49959057+joaopedrolibonati@users.noreply.github.com>
2019-10-02 03:13:21 +0000

    remove api keys

--- API_KEYs.js
+++ API_KEYs.js
@@ -1,7 +1,7 @@
-exports.imagem_API_KEY = 'B7FDJUoGyN6OS3jtjHNt7MKl2Pl6teIoMRgbtiXs230S';
-exports.tradutor_API_KEY = 'VwUyYA6fS5jZvKuPhXEMOX06TkieiIBpj0PReksaYwmB';
+exports.imagem_API_KEY = '<API_KEY>';
+exports.tradutor_API_KEY = '<API_KEY>';
 exports.watson = {
-    "API_KEY": "_pXDMfAu1t1_ElDaRWo1WFBqrlnSWzK1V3R45AR4Iu9L",
-    "workspace_id": "2422ee5a-ffe9-4d3c-8965-dc6c39c2b362",
-    "assistant_id": "e4abebb6-3457-4c03-9b08-41a065039ec9",
+    "API_KEY": "<API_KEY>",
+    "workspace_id": "<WORKSPACE_KEY>",
+    "assistant_id": "<ASSISTANT_KEY>",
 };
