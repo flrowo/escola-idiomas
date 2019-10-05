a1369f2745f5b1f47089639cb443f010444ca2db
João Pedro Libonati <49959057+joaopedrolibonati@users.noreply.github.com>
2019-10-05 21:22:07 +0000

    Delete API_KEYs.js

--- API_KEYs.js
+++
@@ -1,7 +0,0 @@
-exports.imagem_API_KEY = '<API_KEY>';
-exports.tradutor_API_KEY = '<API_KEY>';
-exports.watson = {
-    "API_KEY": "<API_KEY>",
-    "workspace_id": "<WORKSPACE_KEY>",
-    "assistant_id": "<ASSISTANT_KEY>",
-};
